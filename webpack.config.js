abab078d9269ecd7ef27ee411ab3521b83ddc311
Sujit <psujit14@gmail.com>
2017-11-10 15:21:22 +0100

    Updated source map type

--- webpack.config.js
+++ webpack.config.js
@@ -146,7 +146,7 @@ if (TARGET !== undefined && TARGET.startsWith('build')) {
       chunkFilename: '[name].[hash].js'
     },
 	
-	devtool: 'cheap-eval-source-map',
+	devtool: 'eval',
 
     module: {
       rules: [
